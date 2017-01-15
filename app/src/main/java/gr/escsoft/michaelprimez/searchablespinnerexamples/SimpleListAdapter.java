cdfc15fdc93f16407e6134ffc685e7d8c8697126
Michael <michael.primez@gmail.com>
2017-01-15 22:36:14 +0100

    add gif

--- app/src/main/java/gr/escsoft/michaelprimez/searchablespinnerexamples/SimpleListAdapter.java
+++ app/src/main/java/gr/escsoft/michaelprimez/searchablespinnerexamples/SimpleListAdapter.java
@@ -9,7 +9,6 @@ import android.widget.BaseAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.amulyakhare.textdrawable.TextDrawable;
